b30ab1511c14ca78c764bf3422a58ad6f2601d64
Lyu Pengfei <pf_lyu@qq.com>
2020-09-02 15:10:42 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,120 @@
-﻿/// <reference types="epg-panel" />
+﻿/// <reference types="@rpdg/epg-panel" />
 
 x$.log.active(true);
 
-let container = document.getElementById('divTest');
-let dataItems = x$.dom.select('#divTest>.divItem');
-let overCount = 0;
-let divItemLength = 17;
+let a = x$
+	.grid<{
+		container: HTMLElement,
+		dataItems: HTMLElement[],
+		overCount: number,
+		divItemLength: number,
+	}>('#divTest>.curRow', {
+		cols: 7,
+		frameId: 'divHelper',
+		edgeRule: {
+			down: function () {
+				let nodeIdIndex = parseInt(a.selectedElement.id.substr(5), 10);
 
-let a = x$.grid('#divTest>.curRow', {
-	cols: 7,
-	frameId: 'divHelper',
-	edgeRule: {
-		down: function () {
-			let nodeIdIndex = parseInt(a.selectedElement.id.substr(5), 10);
-
-			if (nodeIdIndex + a.cols < divItemLength) {
-				overCount++;
-				for (let i = 0, len = a.items.length; i < len; i++) {
-					a.items[i].className = 'divItem';
-					if (i + overCount * a.cols < dataItems.length) {
-						dataItems[i + overCount * this.cols].className = 'divItem curRow';
+				if (nodeIdIndex + a.cols < a.ex.divItemLength) {
+					a.ex.overCount++;
+					for (let i = 0, len = a.items.length; i < len; i++) {
+						a.items[i].className = 'divItem';
+						if (i + a.ex.overCount * a.cols < a.ex.dataItems.length) {
+							a.ex.dataItems[i + a.ex.overCount * this.cols].className = 'divItem curRow';
+						}
 					}
+					a.ex.container.style.marginTop = -40 * a.ex.overCount + 'px';
+					a.reset(a.selectedIndex);
+					//console.log('down:---' , this.items);
 				}
-				container.style.marginTop = -40 * overCount + 'px';
-				a.reset(a.selectedIndex);
-				//console.log('down:---' , this.items);
+			},
+			up: function () {
+				let nodeIdIndex = parseInt(a.selectedElement.id.substr(5), 10);
+				if (a.ex.overCount > 0) {
+					a.ex.overCount--;
+					for (let i = 0, len = a.items.length; i < len; i++) {
+						a.items[i].className = 'divItem';
+					}
+
+					let t = ((nodeIdIndex / a.cols) | 0) - 1;
+					for (let j = 0; j < a.cols; j++) {
+						a.ex.dataItems[t * a.cols + j].className = 'divItem curRow';
+					}
+					a.ex.container.style.marginTop = -40 * a.ex.overCount + 'px';
+					a.reset(a.selectedIndex);
+					//console.log('up:--- ' , this.items);
+				}
+			},
+			left: function () {
+				a.jumpToBox(1);
+			},
+		},
+		onOk: function () {
+			console.log(a.selectedIndex, this.selectedElement);
+		},
+	})
+	.extra({
+		container: document.getElementById('divTest'),
+		dataItems: x$.dom.select('#divTest>.divItem'),
+		overCount: 0,
+		divItemLength: 17,
+	});
+
+let b = x$
+	.grid<{
+		overCount: number,
+		fullIemList: HTMLElement[],
+	}>('#divTest2>.current', {
+		cols: 4,
+		frameId: 'divHelper2',
+		offset: { x: -2, y: -2 },
+		forceRec: { w: 40, h: 40, x: 17, y: 297 },
+		onChange: function (w) {
+			if (w != 'auto') {
+				let obj = b.selectedElement.firstChild as HTMLElement;
+				obj.className = 'z1';
+				obj = b.previousElement;
+				if (obj) {
+					(obj.firstChild as HTMLElement).className = 'z0';
+				}
 			}
 		},
-		up: function () {
-			let nodeIdIndex = parseInt(a.selectedElement.id.substr(5), 10);
-			if (overCount > 0) {
-				overCount--;
-				for (let i = 0, len = a.items.length; i < len; i++) {
-					a.items[i].className = 'divItem';
+		edgeRule: {
+			right: function (d) {
+				if (this.selectedIndex + b.ex.overCount + 1 < b.ex.fullIemList.length) {
+					b.ex.overCount++;
+
+					this.items[0].className = 'divItem';
+					b.ex.fullIemList[0].style.marginLeft = -b.ex.overCount * 40 + 'px';
+					b.ex.fullIemList[this.selectedIndex + b.ex.overCount].className = 'divItem current';
+					this.reset(this.selectedIndex);
+					//
+					let obj = this.items[this.selectedIndex - 1].firstChild as HTMLElement;
+					obj.className = 'z0';
 				}
+			},
+			left: function () {
+				if (this.selectedIndex + b.ex.overCount - 1 > -1) {
+					b.ex.overCount--;
 
-				let t = ((nodeIdIndex / a.cols) | 0) - 1;
-				for (let j = 0; j < a.cols; j++) {
-					dataItems[t * a.cols + j].className = 'divItem curRow';
+					this.items[this.cols - 1].className = 'divItem';
+					b.ex.fullIemList[0].style.marginLeft = -b.ex.overCount * 40 + 'px';
+					b.ex.fullIemList[this.selectedIndex + b.ex.overCount].className = 'divItem current';
+					this.reset(this.selectedIndex);
+					//
+					let obj = this.items[this.selectedIndex + 1].firstChild as HTMLElement;
+					obj.className = 'z0';
 				}
-				container.style.marginTop = -40 * overCount + 'px';
-				a.reset(a.selectedIndex);
-				//console.log('up:--- ' , this.items);
-			}
+			},
+			up: function () {
+				this.jumpToBox(0);
+			},
+			down: 'stop',
 		},
-		left: function () {
-			a.jumpToBox(1);
-		},
-	},
-	onOk: function () {
-		console.log(a.selectedIndex, this.selectedElement);
-	},
-});
-
-let b = x$.grid('#divTest2>.current', {
-	cols: 4,
-	frameId: 'divHelper2',
-	offset: { x: -2, y: -2 },
-	forceRec: { w: 40, h: 40, x: 17, y: 297 },
-	onChange: function (w) {
-		if (w != 'auto') {
-			let obj = b.selectedElement.firstChild as HTMLElement;
-			obj.className = 'z1';
-			obj = b.previousElement;
-			if (obj) {
-				(obj.firstChild as HTMLElement).className = 'z0';
-			}
-		}
-	},
-	edgeRule: {
-		// right: function (d) {
-		// 	if (b.selectedIndex + this.overCount + 1 < this.fullIemList.length) {
-		// 		this.overCount++;
-
-		// 		this.items[0].className = 'divItem';
-		// 		this.fullIemList[0].style.marginLeft = -this.overCount * 40 + 'px';
-		// 		this.fullIemList[this.selectedIndex + this.overCount].className = 'divItem current';
-		// 		this.reset(this.selectedIndex);
-		// 		//
-		// 		var obj = this.items[this.selectedIndex - 1].firstChild;
-		// 		obj.className = 'z0';
-		// 	}
-		// },
-	},
-});
+	})
+	.extra({
+		overCount: 0,
+		fullIemList: x$.dom.select('#divTest2>.divItem'),
+	});
 
 x$.box.addGrid(a, b).active();
