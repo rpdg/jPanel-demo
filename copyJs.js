dd392752773770a978a2b7be7e546afbe6cbb1ab
Lyu Pengfei <pf_lyu@qq.com>
2020-09-03 10:18:19 +0800

    add js

---
+++ copyJs.js
@@ -0,0 +1,9 @@
+﻿const fs = require('fs');
+
+// destination.txt will be created or overwritten by default.
+fs.copyFile('./node_modules/@rpdg/epg-panel/dist/index.bundle.js', './src/js/epg.min.js', (err) => {
+	if (err) {
+		throw err;
+	}
+	console.log('epg.js was copied to src directory');
+});
